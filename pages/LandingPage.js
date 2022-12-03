e2a84cc692592a36fc85374ab3c428da73149261
shivansh <shivanshkalra796@gmail.com>
2022-12-03 21:24:24 +0000

    Landing page prepped

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -17,13 +17,32 @@ async function connectWallet() {
   console.log("EOA address", accounts);
 }
 
-
-
-
 const LandingPage = () => {
   const router = useRouter();
   return (
-    <div className="main flex flex-col justify-center items-center text-black">
+    <div
+      className="main flex flex-col justify-center items-center text-black"
+      style={{ backgroundImage: `url(${Kort})` }}
+    >
+      <div class="flex">
+      <div class="flex-none ">
+        <Image src={myLogo} />
+        <p style={{ fontStyle: "sans-serrif" }}>Lorem Ipsum Caption Caption</p>
+      </div>
+      <div class="flex-initial">
+        <h1>Our Aim and goal through our product</h1>
+        <h4>
+          Lorem Ipsum Lorem IpsumLorem Ipsum Lorem IpsumLorem Ipsum Lorem
+          IpsumLorem Ipsum Lorem IpsumLorem Ipsum Lorem IpsumLorem Ipsum Lorem
+          IpsumLorem Ipsum Lorem IpsumLorem Ipsum Lorem IpsumLorem Ipsum Lorem
+          IpsumLorem Ipsum Lorem IpsumLorem Ipsum Lorem IpsumLorem Ipsum Lorem
+          IpsumLorem Ipsum Lorem Ipsum{" "}
+        </h4>
+<p>Feel like you are all set to roll? </p>
+
+      </div>
+      <button>Dive right</button> 
+      </div>
       <button
         className="w-20 h-10 bg-red-300"
         onClick={() => {
@@ -33,7 +52,8 @@ const LandingPage = () => {
       >
         Enter Dapp
       </button>
-    </div>
+      </div>
+    
   );
 };
 
